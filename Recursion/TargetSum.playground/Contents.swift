7df661fe104f770208df91d3fe1b25f256d9e0e4
Manohar Simons <>
2019-11-29 23:06:09 -0800

    Target Sum: Was able to solve this problem in 11 minutes

--- Recursion/TargetSum.playground/Contents.swift
+++ Recursion/TargetSum.playground/Contents.swift
@@ -1,27 +1,23 @@
-
 func targetSum(arr: [Int], target: Int) -> Bool {
-    var result:[String] = []
+    var result:[[Int]] = []
     var slate:[Int] = []
-    targetSumHelper(slate: &slate, bag: arr, index: 0, target: target, sum: 0, result: &result)
-    //print(result)
+    targetSumHelper(slate: &slate, bag: arr, index: 0, target: target, intermediateSum: 0, result: &result)
     return result.count != 0
 }
 
-func targetSumHelper(slate: inout [Int], bag: [Int], index: Int, target: Int, sum: Int, result: inout [String]) {
-    if sum == target && slate.count != 0 {
-        result.append("\(slate)")
+func targetSumHelper(slate: inout [Int], bag: [Int], index: Int, target: Int, intermediateSum: Int, result: inout [[Int]]) {
+    if intermediateSum == target && slate.count != 0 {
+        result.append(slate)
         return
     } else if index == bag.count {
         return
     } else {
-        targetSumHelper(slate: &slate, bag: bag, index: index+1, target: target, sum: sum, result: &result)
+        targetSumHelper(slate: &slate, bag: bag, index: index+1, target: target, intermediateSum: intermediateSum, result: &result)
         
         slate.append(bag[index])
-        targetSumHelper(slate: &slate, bag: bag, index: index+1, target: target, sum: sum+bag[index], result: &result)
+        targetSumHelper(slate: &slate, bag: bag, index: index+1, target: target, intermediateSum: intermediateSum+bag[index], result: &result)
         slate.popLast()
     }
 }
 
-var arr = [2,4,6]
-var result = targetSum(arr: arr, target: 0)
-print(result)
+print(targetSum(arr: [2,4,6], target: 6))
