31f9196ae282b96c256781377e11efa584021d48
Manohar Simons <>
2019-12-18 12:25:37 +0530

    reduced the space complexity for O(1)

--- DynamicProgramming/1.IntroductionVideos/CountingSubsets.playground/Contents.swift
+++ DynamicProgramming/1.IntroductionVideos/CountingSubsets.playground/Contents.swift
@@ -1,25 +1,21 @@
 func combination(n: Int, k: Int) -> Int {
-    var array: [[Int]] = [[Int]](repeating: Array(repeating: 0, count: k+1), count: n+1)
+    var array: [[Int]] = [[Int]](repeating: Array(repeating: 0, count: k+1), count: 2)
     
     if k==0 || k==n {
         return 1
     }
     
-    for row in 0...n {
-        array[row][0] = 1
-    }
-    
-    for i in 0...k {
-        array[i][i] = 1
-    }
+    array[0][0] = 1
+    array[1][0] = 1
+    array[1][1] = 1
     
     for row in 2...n {
         for col in 1...min(row, k) {
-            array[row][col] = array[row-1][col-1] + array[row-1][col]
+            array[row%2][col] = array[(row-1)%2][col-1] + array[(row-1)%2][col]
         }
     }
     
-    return array[n][k]
+    return array[n%2][k]
 }
 
 print(combination(n: 10, k: 7))
