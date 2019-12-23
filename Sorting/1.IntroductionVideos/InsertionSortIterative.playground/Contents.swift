7810c6b28292a5ab9e0559c7290081b050e35bab
Manohar Simons <>
2019-12-23 21:45:47 +0530

    Insertion Sort - Iterative

---
+++ Sorting/1.IntroductionVideos/InsertionSortIterative.playground/Contents.swift
@@ -0,0 +1,21 @@
+func insertionSort(n: Int, array: inout [Int]) {
+    for i in 1...n-1 {
+        var count = i
+        while array[count] < array[count-1] {
+            let temp = array[count]
+            array[count] = array[count-1]
+            array[count-1] = temp
+            count-=1
+        }
+    }
+}
+
+
+func insertionSortHelper(array: [Int]) -> [Int] {
+    var array = array
+    insertionSort(n: array.count, array: &array)
+    return array
+}
+
+print(insertionSortHelper(array: [1,3454,9834,972,36,465,78,2435,123568,123,45,123,45]))
+
